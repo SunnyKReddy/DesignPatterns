83c66f604fc1890a2c8c57c068c6bd7062200a5d
SunnyKReddy <sunnykousik1997@gmail.com>
2023-07-25 17:56:36 +0530

    Design Patterns - Strategy Design Pattern Type 2 (Travel) Mode of Transport 25/07/2023 17:56

--- src/StrategyDesignPattern/StrategyDesignPatternType2/Client.java
+++ src/StrategyDesignPattern/StrategyDesignPatternType2/Client.java
@@ -5,5 +5,7 @@ public class Client {
         Journey journey1 = new Journey("Hyderabad", "Bangalore", ModeOfTransport.CAR);
 
         Journey journey2 = new Journey("Bangalore", "Chennai", ModeOfTransport.BUS);
+
+        Journey journey3 = new Journey("Goa", "Kerala", ModeOfTransport.BIKE);
     }
 }
