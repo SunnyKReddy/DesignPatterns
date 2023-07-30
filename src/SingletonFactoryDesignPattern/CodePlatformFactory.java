b4a3d91177723742e213622d3eb9d46dc33ae5d6
SunnyKReddy <sunnykousik1997@gmail.com>
2023-07-30 08:00:50 +0530

    Design Patterns : SingletonFactoryDesignPattern, comments added -> 30/07/2023 8:00

--- src/SingletonFactoryDesignPattern/CodePlatformFactory.java
+++ src/SingletonFactoryDesignPattern/CodePlatformFactory.java
@@ -1,6 +1,7 @@
 package SingletonFactoryDesignPattern;
 
 public class CodePlatformFactory {
+//    private static CodePlatformFactory codePlatformFactoryInstance;
     private String codePlatformType;
     private CodePlatform codePlatformInstance;
 
@@ -9,11 +10,27 @@ public class CodePlatformFactory {
         codePlatformObjectFactory(this.codePlatformType);
     }
 
+//    private CodePlatformFactory(String codePlatformType) {
+//        this.codePlatformType = codePlatformType;
+//    }
+
+//    public static CodePlatform getCodePlatformFactoryInstance(String codePlatformType){
+//        if(codePlatformFactoryInstance == null) {
+//            synchronized (CodePlatformFactory.class) {
+//                if(codePlatformFactoryInstance == null) {
+//                    CodePlatformFactory.codePlatformFactoryInstance = new CodePlatformFactory(codePlatformType);
+//                }
+//            }
+//        }
+//        CodePlatformFactory.codePlatformObjectFactory(CodePlatformFactory.codePlatformType);
+//        return CodePlatformFactory.codePlatformInstance;
+//    }
+
     private void codePlatformObjectFactory(String codePlatformType) {
         switch (codePlatformType) {
-            case "JAVA": this.codePlatformInstance = JavaCodePlatform.getJavaCodePlatformInstance(); break;
-            case "PYTHON": this.codePlatformInstance = new PythonCodePlatform(); break;
-            case "CPP": this.codePlatformInstance = CPPCodePlatform.getCPPCodePlatformInstance(); break;
+            case "JAVA": this.codePlatformInstance = JavaCodePlatform.getJavaCodePlatformInstance(); break; //For every execution of a Java code only single platform object will be created
+            case "PYTHON": this.codePlatformInstance = new PythonCodePlatform(); break; //For every execution of a Python code, different platform objects will be created
+            case "CPP": this.codePlatformInstance = CPPCodePlatform.getCPPCodePlatformInstance(); break; //For every execution of a CPP code only single platform object will be created
         }
     }
 
